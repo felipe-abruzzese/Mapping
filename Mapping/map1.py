0102a2cd402fcf55f77bb944a31a65bff6d49609
Felipe <31362855+felipe-abruzzese@users.noreply.github.com>
2019-04-07 23:42:26 -0300

    Add Função Lambda e controle de layer

--- Mapping/map1.py
+++ Mapping/map1.py
@@ -22,9 +22,13 @@ fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev): #A função zip() permite passar por duas listas em sequência tipo: [a1, a2, a3] e [b1, b2, b3] o resultado será: [a1, b1, a2, b2, a3, b3]
     iframe = folium.IFrame(html=html % str(el), width=200, height=100)
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=folium.Popup(iframe), fill_color= color_producer(el), color = 'grey', fill_opacity=0.7))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=folium.Popup(iframe),
+    fill_color= color_producer(el), color = 'grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange'
+if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'})) # A função Lambda é uma função que funciona em uma linha só
 
 map.add_child(fg)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
